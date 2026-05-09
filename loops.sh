3d51896dc0a6be2ad6eff5216b71aa1f0d7b6898
Abdul Mohammed <abdulmohammed0212@gmail.com>
2026-05-08 21:16:41 -0400

    Added variables and loops script

---
+++ loops.sh
@@ -0,0 +1,18 @@
+
+
+#!/bin/bash
+name="Abdul"
+job="Cloud Engineer"
+echo "My name is $name"
+echo "I am becoming a $job"
+echo "-------------------"
+echo "Counting to 5:"
+for i in 1 2 3 4 5
+do
+echo "Number: $i"
+done
+echo "Cloud tools I will learn:"
+for tool in Linux Git Docker AWS Kubernetes
+do
+echo "-$tool"
+done
